f437b5221e94adb4e6bd5c884133925a2ff9453c
Fabio <fabio@jumpcomm.it>
2020-12-30 14:52:35 +0100

    Observer e Monkey before oilstub

--- src/oilstub.js
+++ src/oilstub.js
@@ -1,2 +1,14 @@
+import { observer } from './scripts/element-observer/observer';
+import { monkey } from './scripts/element-observer/monkey';
+
+if (window.CLIENT_SIDE_BLOCKING.active) {
+    // Starts the monitoring
+    observer.observe(document.documentElement, {
+        childList: true,
+        subtree: true
+    });
+    monkey();
+  }
+
 const cmpstub = require('didomi-iabtcf-stub');
 cmpstub();

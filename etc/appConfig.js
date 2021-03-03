43699f63b5f7f854f4306e2d2fcc1ed5ea973764
Fabio <fabio@jumpcomm.it>
2021-03-03 17:38:01 +0100

    Added extra style inline

--- etc/appConfig.js
+++ etc/appConfig.js
@@ -172,6 +172,12 @@ module.exports = {
     chunksSortMode: 'dependency',
     inject: 'head'
   },{
+    filename: 'demos/sisalpay-app-avacy-demo.html',
+    template: path.resolve(sourcePath, 'demos', 'sisalpay-app-avacy-demo.html'),
+    chunks: ['oilstub', 'oil'],
+    chunksSortMode: 'dependency',
+    inject: 'head'
+  },{
     filename: 'demos/open-source-example.html',
     template: path.resolve(sourcePath, 'demos', 'open-source-example.html'),
     chunks: [],
